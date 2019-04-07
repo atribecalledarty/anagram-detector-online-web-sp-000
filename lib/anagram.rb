0ce31211d1da26f6bd0ec40bc6da219c4763fb91
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 04:46:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,8 +4,10 @@ class Anagram
   
   def match(array_of_words)
     my_sorted_word_array = @word.split("").sort
-    array_of_words.each do |word|
-      
+    
+    array_of_words.collect do |word|
+      sorted_word_array = word.split("").sort
+      my_sorted_word_array = sorted_word_array
     end
     
   end

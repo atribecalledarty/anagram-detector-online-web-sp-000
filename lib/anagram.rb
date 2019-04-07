0d76818618c3f5c99d90f92bc282869e7181cd10
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 04:44:37 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def match(array_of_words)
+    my_sorted_word_array = @word.split("").sort
+    array_of_words.each do |word|
+      
+    end
+    
+  end
+end

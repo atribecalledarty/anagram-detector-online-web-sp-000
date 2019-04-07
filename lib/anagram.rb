677952004fa5ee0a1311e821344992d97c74d20f
Arthur Song <arth3rs0ng@gmail.com>
2019-04-07 04:47:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   def match(array_of_words)
     my_sorted_word_array = @word.split("").sort
     
-    array_of_words.collect do |word|
+    array_of_words.select do |word|
       sorted_word_array = word.split("").sort
       my_sorted_word_array == sorted_word_array
     end
